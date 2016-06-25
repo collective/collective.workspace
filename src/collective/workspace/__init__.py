0c30057c3b9ade293f9bf1c5167b4e525dcab33e
David Glick <david@glicksoftware.com>
2016-06-25 12:10:34 -0700

    remove obsolete PAS plugin

--- src/collective/workspace/__init__.py
+++ src/collective/workspace/__init__.py
@@ -1,19 +1,2 @@
 from zope.i18nmessageid import MessageFactory
 workspaceMessageFactory = MessageFactory('collective.workspace')
-
-from AccessControl.Permissions import add_user_folders
-from collective.workspace import pas
-from Products.PluggableAuthService import registerMultiPlugin
-
-registerMultiPlugin(pas.WorkspaceGroupManager.meta_type)
-
-
-def initialize(context):
-
-    context.registerClass(
-        pas.WorkspaceGroupManager,
-        permission=add_user_folders,
-        constructors=(pas.manage_addWorkspaceGroupManagerForm,
-                      pas.addWorkspaceGroupManager),
-        visibility=None
-        )
