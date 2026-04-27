0be86b03c507245605ff10484b37c8048ce4d27e
ale-rt <alessandro.pisa@gmail.com>
2026-04-27 08:24:32 +0200

    Back to development: 4.0.0a2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = "4.0.0a1"
+version = "4.0.0a2.dev0"
 
 long_description = (
     open("README.rst").read() + "\n" + "Contributors\n"
